313aa556ee3d8d080a60cb7c3d6570e79fb11a2f
ta-ulyanava <ta.ulyanava@gmail.com>
2025-02-15 18:12:48 +0100

    Add Spec

---
+++ src/main/java/com/example/teamcity/api/spec/Specifications.java
@@ -0,0 +1,32 @@
+package com.example.teamcity.api.spec;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+
+import java.util.List;
+
+public class Specifications {
+    private static Specifications spec;
+
+    private Specifications() {
+
+    }
+
+    public static Specifications getSpec() {
+        if (spec == null) {
+            spec = new Specifications();
+        }
+        return spec;
+    }
+
+    private RequestSpecBuilder reqBuilder() {
+        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+        reqBuilder.setContentType(ContentType.JSON);
+        reqBuilder.setAccept(ContentType.JSON);
+        reqBuilder.addFilters(List.of(new RequestLoggingFilter(), new ResponseLoggingFilter()));
+        return reqBuilder;
+    }
+    //https://youtu.be/Sj4RDSWyl-g?t=1177
+}
