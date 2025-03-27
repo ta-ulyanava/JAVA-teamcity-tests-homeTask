6620fa9464116f506ff34a0f5ad4cc78333df1e0
ta-ulyanava <ta.ulyanava@gmail.com>
2025-03-27 12:40:48 +0100

    Add listeners

--- src/test/java/com/example/teamcity/BaseTest.java
+++ src/test/java/com/example/teamcity/BaseTest.java
@@ -6,10 +6,11 @@ import com.example.teamcity.api.requests.CheckedRequest;
 import com.example.teamcity.api.spec.request.RequestSpecs;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.asserts.SoftAssert;
 
 import static com.example.teamcity.api.generators.TestDataGenerator.generate;
-
+@Listeners({io.qameta.allure.testng.AllureTestNg.class})
 public class BaseTest {
     protected SoftAssert softy;
     protected TestData testData;
--- src/test/java/com/example/teamcity/ui/BaseUiTest.java
+++ src/test/java/com/example/teamcity/ui/BaseUiTest.java
@@ -2,11 +2,13 @@ package com.example.teamcity.ui;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.example.teamcity.BaseTest;
 import com.example.teamcity.api.config.Config;
 import com.example.teamcity.api.enums.ApiEndpoint;
 import com.example.teamcity.api.models.User;
 import com.example.teamcity.ui.pages.LoginPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
@@ -15,10 +17,10 @@ import java.util.Map;
 
 import static io.qameta.allure.Allure.step;
 
-@Listeners({io.qameta.allure.testng.AllureTestNg.class})
 public class BaseUiTest extends BaseTest {
     @BeforeSuite(alwaysRun = true)
     public void setupUiTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide().screenshots(true).savePageSource(true));
         Configuration.browser = Config.getProperty("browser");
         Configuration.baseUrl = "http://" + Config.getProperty("host");
         Configuration.remote= Config.getProperty("remote");
