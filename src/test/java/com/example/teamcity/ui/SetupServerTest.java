0bc2624496dcfbfee251038095b7a8a464c16248
ta-ulyanava <ta.ulyanava@gmail.com>
2025-03-29 22:14:00 +0100

    Fixed setup test

--- src/test/java/com/example/teamcity/ui/SetupServerTest.java
+++ src/test/java/com/example/teamcity/ui/SetupServerTest.java
@@ -2,7 +2,6 @@ package com.example.teamcity.ui;
 
 import com.example.teamcity.ui.setup.FirstStartPage;
 import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 
@@ -10,7 +9,10 @@ import org.testng.annotations.Test;
 public class SetupServerTest extends BaseUiTest {
     @Description("Verifies that user can complete initial TeamCity setup: license acceptance and DB config.")
     @Test(groups = {"Setup"})
-    public void setupTeamCityServerTest(){
-        FirstStartPage.open().setupFirstStart();
+    public void setupTeamCityServerTest() {
+        FirstStartPage page = FirstStartPage.open();
+        if (page.isAtFirstStartPage()) {
+            page.setupFirstStart();
+        }
     }
 }
